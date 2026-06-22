eb0be4845a10d69eac06d750bacd47ec22b89938
cvenga1 <chris.vengalottue@yahoo.com>
2026-06-22 19:23:11 +0100

    Update healthchecker.sh

--- project_bash/healthchecker.sh
+++ project_bash/healthchecker.sh
@@ -6,12 +6,16 @@ echo "---Health Checker----------"
 echo ""
 echo "----Disk Usage---"
 df -h
+#shows disk usage
 
 echo ""
 echo "-----Memory Usage----"
 free -m
+#shows memory usgae
 
 echo ""
 
 echo "======Top Process ==="
 ps aux --sort=%mem | head
+#shows running process and sorts them highest memory users first
+#head- first 10
