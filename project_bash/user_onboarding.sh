bd930064e4eb4a9232de530d7505dd34bcefb074
cvenga1 <chris.vengalottue@yahoo.com>
2026-06-22 19:21:29 +0100

    Update user_onboarding.sh

--- project_bash/user_onboarding.sh
+++ project_bash/user_onboarding.sh
@@ -3,9 +3,13 @@
 USERNAME=$1
 
 sudo useradd -m $USERNAME
+#creates a useraccount and home directory
 
 sudo passwd $USERNAME
+#prompts the user to set password
 
 sudo usermod -aG sudo $USERNAME
+#adds groups to the sudoer group
 
 echo "User $USERNAME created successfully"
+#prints msg to confirm
